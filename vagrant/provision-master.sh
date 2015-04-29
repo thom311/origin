thom311/origin

commit 40be29beaabd097788200d8dadbf0a9e35374df7

make openshift start --write-config take a dir

vagrant/provision-master.sh
@@ -33,7 +33,10 @@ popd
 # Initialize certificates
 echo "Generating certs"
 pushd /vagrant
-  CERT_DIR=`pwd`/openshift.local.certificates
+  SERVER_CONFIG_DIR="`pwd`/openshift.local.config"
+  MASTER_CONFIG_DIR="${SERVER_CONFIG_DIR}/master"
+  NODE_CONFIG_DIR="${SERVER_CONFIG_DIR}/node-${minion}"
+  CERT_DIR="${MASTER_CONFIG_DIR}"
 
   # Master certs
   /usr/bin/openshift admin create-master-certs \
@@ -48,7 +51,7 @@ pushd /vagrant
     ip=${minion_ip_array[$i]}
 
     /usr/bin/openshift admin create-node-config \
-      --node-dir="${CERT_DIR}/node-${minion}" \
+      --node-dir="${NODE_CONFIG_DIR}" \
       --node="${minion}" \
       --hostnames="${minion},${ip}" \
       --master="https://${MASTER_IP}:8443" \
@@ -86,12 +89,12 @@ systemctl start openshift-master.service
 
 # if SDN requires service on master, then set it up
 if [ "${OPENSHIFT_SDN}" != "ovs-gre" ]; then
-  export ETCD_CAFILE=/vagrant/openshift.local.certificates/ca/cert.crt
-  export ETCD_CERTFILE=/vagrant/openshift.local.certificates/master/etcd-client.crt
-  export ETCD_KEYFILE=/vagrant/openshift.local.certificates/master/etcd-client.key
+  export ETCD_CAFILE=/vagrant/openshift.local.config/master/ca.crt
+  export ETCD_CERTFILE=/vagrant/openshift.local.config/master/master.etcd-client.crt
+  export ETCD_KEYFILE=/vagrant/openshift.local.config/master/master.etcd-client.key
   $(dirname $0)/provision-master-sdn.sh $@
 fi
 
 # Set up the OPENSHIFTCONFIG environment variable for use by osc
-echo 'export OPENSHIFTCONFIG=/vagrant/openshift.local.certificates/admin/.kubeconfig' >> /root/.bash_profile
-echo 'export OPENSHIFTCONFIG=/vagrant/openshift.local.certificates/admin/.kubeconfig' >> /home/vagrant/.bash_profile
+echo 'export OPENSHIFTCONFIG=/vagrant/openshift.local.config/master/admin.kubeconfig' >> /root/.bash_profile
+echo 'export OPENSHIFTCONFIG=/vagrant/openshift.local.config/master/admin.kubeconfig' >> /home/vagrant/.bash_profile

hack/export-certs.sh
@@ -3,7 +3,7 @@
 # This command attempts to export the correct arguments for a curl client.
 # Exports CURL_ARGS which should be used with curl:
 #
-#   $ source hack/export-certs.sh ./openshift.local.certificates/admin
+#   $ source hack/export-certs.sh ./openshift.local.config/master/admin
 #   $ curl $CURL_ARGS <a protected URL>
 
 set -o errexit

hack/test-cmd.sh
@@ -50,9 +50,12 @@ KUBELET_PORT=${KUBELET_PORT:-10250}
 TEMP_DIR=${USE_TEMP:-$(mktemp -d /tmp/openshift-cmd.XXXX)}
 ETCD_DATA_DIR="${TEMP_DIR}/etcd"
 VOLUME_DIR="${TEMP_DIR}/volumes"
-CERT_DIR="${TEMP_DIR}/certs"
+FAKE_HOME_DIR="${TEMP_DIR}/openshift.local.home"
+SERVER_CONFIG_DIR="${TEMP_DIR}/openshift.local.config"
+MASTER_CONFIG_DIR="${SERVER_CONFIG_DIR}/master"
+NODE_CONFIG_DIR="${SERVER_CONFIG_DIR}/node-${KUBELET_HOST}"
 CONFIG_DIR="${TEMP_DIR}/configs"
-mkdir -p "${ETCD_DATA_DIR}" "${VOLUME_DIR}" "${CERT_DIR}" "${CONFIG_DIR}"
+mkdir -p "${ETCD_DATA_DIR}" "${VOLUME_DIR}" "${FAKE_HOME_DIR}" "${MASTER_CONFIG_DIR}" "${NODE_CONFIG_DIR}" "${CONFIG_DIR}"
 
 # handle profiling defaults
 profile="${OPENSHIFT_PROFILE-}"
@@ -77,7 +80,7 @@ echo openshift: $out
 export OPENSHIFT_PROFILE="${WEB_PROFILE-}"
 
 # Specify the scheme and port for the listen address, but let the IP auto-discover. Set --public-master to localhost, for a stable link to the console.
-echo "[INFO] Create certificates for the OpenShift server to ${CERT_DIR}"
+echo "[INFO] Create certificates for the OpenShift server to ${MASTER_CONFIG_DIR}"
 # find the same IP that openshift start will bind to.  This allows access from pods that have to talk back to master
 ALL_IP_ADDRESSES=`ifconfig | grep "inet " | sed 's/adr://' | awk '{print $2}'`
 SERVER_HOSTNAME_LIST="${PUBLIC_MASTER_HOST},localhost"
@@ -88,42 +91,51 @@ done <<< "${ALL_IP_ADDRESSES}"
 
 openshift admin create-master-certs \
   --overwrite=false \
-  --cert-dir="${CERT_DIR}" \
+  --cert-dir="${MASTER_CONFIG_DIR}" \
   --hostnames="${SERVER_HOSTNAME_LIST}" \
   --master="${MASTER_ADDR}" \
   --public-master="${API_SCHEME}://${PUBLIC_MASTER_HOST}"
 
 openshift admin create-node-config \
   --listen="${KUBELET_SCHEME}://0.0.0.0:${KUBELET_PORT}" \
-  --node-dir="${CERT_DIR}/node-${KUBELET_HOST}" \
+  --node-dir="${NODE_CONFIG_DIR}" \
   --node="${KUBELET_HOST}" \
   --hostnames="${KUBELET_HOST}" \
   --master="${MASTER_ADDR}" \
-  --node-client-certificate-authority="${CERT_DIR}/ca/cert.crt" \
-  --certificate-authority="${CERT_DIR}/ca/cert.crt" \
-  --signer-cert="${CERT_DIR}/ca/cert.crt" \
-  --signer-key="${CERT_DIR}/ca/key.key" \
-  --signer-serial="${CERT_DIR}/ca/serial.txt"
+  --node-client-certificate-authority="${MASTER_CONFIG_DIR}/ca.crt" \
+  --certificate-authority="${MASTER_CONFIG_DIR}/ca.crt" \
+  --signer-cert="${MASTER_CONFIG_DIR}/ca.crt" \
+  --signer-key="${MASTER_CONFIG_DIR}/ca.key" \
+  --signer-serial="${MASTER_CONFIG_DIR}/ca.serial.txt"
 
-# Start openshift
-OPENSHIFT_ON_PANIC=crash openshift start \
+osadm create-bootstrap-policy-file --filename="${MASTER_CONFIG_DIR}/policy.json"
+
+# create openshift config
+openshift start \
+  --write-config=${SERVER_CONFIG_DIR} \
+  --create-certs=false \
   --master="${API_SCHEME}://${API_HOST}:${API_PORT}" \
   --listen="${API_SCHEME}://${API_HOST}:${API_PORT}" \
   --hostname="${KUBELET_HOST}" \
   --volume-dir="${VOLUME_DIR}" \
-  --cert-dir="${CERT_DIR}" \
-  --etcd-dir="${ETCD_DATA_DIR}" \
-  --create-certs=false 1>&2 &
+  --etcd-dir="${ETCD_DATA_DIR}" 
+
+
+# Start openshift
+OPENSHIFT_ON_PANIC=crash openshift start \
+  --master-config=${MASTER_CONFIG_DIR}/master-config.yaml \
+  --node-config=${NODE_CONFIG_DIR}/node-config.yaml \
+  1>&2 &
 OS_PID=$!
 
 if [[ "${API_SCHEME}" == "https" ]]; then
-    export CURL_CA_BUNDLE="${CERT_DIR}/ca/cert.crt"
-    export CURL_CERT="${CERT_DIR}/admin/cert.crt"
-    export CURL_KEY="${CERT_DIR}/admin/key.key"
+    export CURL_CA_BUNDLE="${MASTER_CONFIG_DIR}/ca.crt"
+    export CURL_CERT="${MASTER_CONFIG_DIR}/admin.crt"
+    export CURL_KEY="${MASTER_CONFIG_DIR}/admin.key"
 fi
 
 # set the home directory so we don't pick up the users .config
-export HOME="${TEMP_DIR}/home"
+export HOME="${FAKE_HOME_DIR}"
 
 wait_for_url "${KUBELET_SCHEME}://${KUBELET_HOST}:${KUBELET_PORT}/healthz" "kubelet: " 0.25 80
 wait_for_url "${API_SCHEME}://${API_HOST}:${API_PORT}/healthz" "apiserver: " 0.25 80
@@ -149,20 +161,20 @@ if [[ "${API_SCHEME}" == "https" ]]; then
 fi
 
 
-osc login --server=${KUBERNETES_MASTER} --certificate-authority="${CERT_DIR}/ca/cert.crt" -u test-user -p anything
+osc login --server=${KUBERNETES_MASTER} --certificate-authority="${MASTER_CONFIG_DIR}/ca.crt" -u test-user -p anything
 osc new-project project-foo --display-name="my project" --description="boring project description"
 [ "$(osc project | grep 'Using project "project-foo"')" ]
 
 
 # test config files from the --config flag
-osc get services --config="${CERT_DIR}/admin/.kubeconfig"
+osc get services --config="${MASTER_CONFIG_DIR}/admin.kubeconfig"
 
 # test config files from env vars
-OPENSHIFTCONFIG="${CERT_DIR}/admin/.kubeconfig" osc get services
+OPENSHIFTCONFIG="${MASTER_CONFIG_DIR}/admin.kubeconfig" osc get services
 
 # test config files in the home directory
 mkdir -p ${HOME}/.config/openshift
-mv ${CERT_DIR}/admin/.kubeconfig ${HOME}/.config/openshift/config
+cp ${MASTER_CONFIG_DIR}/admin.kubeconfig ${HOME}/.config/openshift/config
 osc get services
 mv ${HOME}/.config/openshift/config ${HOME}/.config/openshift/non-default-config
 echo "config files: ok"

hack/test-end-to-end.sh
@@ -45,7 +45,7 @@ if [[ -z "${BASETMPDIR-}" ]]; then
 fi
 ETCD_DATA_DIR="${BASETMPDIR}/etcd"
 VOLUME_DIR="${BASETMPDIR}/volumes"
-CERT_DIR="${BASETMPDIR}/certs"
+FAKE_HOME_DIR="${BASETMPDIR}/openshift.local.home"
 LOG_DIR="${LOG_DIR:-${BASETMPDIR}/logs}"
 ARTIFACT_DIR="${ARTIFACT_DIR:-${BASETMPDIR}/artifacts}"
 mkdir -p $LOG_DIR
@@ -60,6 +60,10 @@ PUBLIC_MASTER_HOST="${PUBLIC_MASTER_HOST:-${API_HOST}}"
 KUBELET_SCHEME="${KUBELET_SCHEME:-https}"
 KUBELET_HOST="${KUBELET_HOST:-127.0.0.1}"
 KUBELET_PORT="${KUBELET_PORT:-10250}"
+
+SERVER_CONFIG_DIR="${BASETMPDIR}/openshift.local.config"
+MASTER_CONFIG_DIR="${SERVER_CONFIG_DIR}/master"
+NODE_CONFIG_DIR="${SERVER_CONFIG_DIR}/node-${KUBELET_HOST}"
 
 # use the docker bridge ip address until there is a good way to get the auto-selected address from master
 # this address is considered stable
@@ -177,7 +181,7 @@ echo "[INFO] `openshift version`"
 echo "[INFO] Server logs will be at:    ${LOG_DIR}/openshift.log"
 echo "[INFO] Test artifacts will be in: ${ARTIFACT_DIR}"
 echo "[INFO] Volumes dir is:            ${VOLUME_DIR}"
-echo "[INFO] Certs dir is:              ${CERT_DIR}"
+echo "[INFO] Config dir is:             ${SERVER_CONFIG_DIR}"
 echo "[INFO] Using images:              ${USE_IMAGES}"
 
 # Start All-in-one server and wait for health
@@ -191,48 +195,57 @@ do
 done <<< "${ALL_IP_ADDRESSES}"
 
 openshift admin create-master-certs \
-  --overwrite=false \
-  --cert-dir="${CERT_DIR}" \
-  --hostnames="${SERVER_HOSTNAME_LIST}" \
-  --master="${MASTER_ADDR}" \
-  --public-master="${API_SCHEME}://${PUBLIC_MASTER_HOST}"
+	--overwrite=false \
+	--cert-dir="${MASTER_CONFIG_DIR}" \
+	--hostnames="${SERVER_HOSTNAME_LIST}" \
+	--master="${MASTER_ADDR}" \
+	--public-master="${API_SCHEME}://${PUBLIC_MASTER_HOST}"
 
 openshift admin create-node-config \
-  --listen="${KUBELET_SCHEME}://0.0.0.0:${KUBELET_PORT}" \
-  --node-dir="${CERT_DIR}/node-${KUBELET_HOST}" \
-  --node="${KUBELET_HOST}" \
-  --hostnames="${KUBELET_HOST}" \
-  --master="${MASTER_ADDR}" \
-  --node-client-certificate-authority="${CERT_DIR}/ca/cert.crt" \
-  --certificate-authority="${CERT_DIR}/ca/cert.crt" \
-  --signer-cert="${CERT_DIR}/ca/cert.crt" \
-  --signer-key="${CERT_DIR}/ca/key.key" \
-  --signer-serial="${CERT_DIR}/ca/serial.txt"
+	--listen="${KUBELET_SCHEME}://0.0.0.0:${KUBELET_PORT}" \
+	--node-dir="${NODE_CONFIG_DIR}" \
+	--node="${KUBELET_HOST}" \
+	--hostnames="${KUBELET_HOST}" \
+	--master="${MASTER_ADDR}" \
+	--node-client-certificate-authority="${MASTER_CONFIG_DIR}/ca.crt" \
+	--certificate-authority="${MASTER_CONFIG_DIR}/ca.crt" \
+	--signer-cert="${MASTER_CONFIG_DIR}/ca.crt" \
+	--signer-key="${MASTER_CONFIG_DIR}/ca.key" \
+	--signer-serial="${MASTER_CONFIG_DIR}/ca.serial.txt"
+
+osadm create-bootstrap-policy-file --filename="${MASTER_CONFIG_DIR}/policy.json"
+
+# create openshift config
+openshift start \
+	--write-config=${SERVER_CONFIG_DIR} \
+	--create-certs=false \
+    --listen="${API_SCHEME}://0.0.0.0:${API_PORT}" \
+    --master="${MASTER_ADDR}" \
+    --public-master="${API_SCHEME}://${PUBLIC_MASTER_HOST}" \
+    --hostname="${KUBELET_HOST}" \
+    --volume-dir="${VOLUME_DIR}" \
+    --etcd-dir="${ETCD_DATA_DIR}" \
+    --images="${USE_IMAGES}"
+
 
 echo "[INFO] Starting OpenShift server"
 sudo env "PATH=${PATH}" OPENSHIFT_PROFILE=web OPENSHIFT_ON_PANIC=crash openshift start \
-     --listen="${API_SCHEME}://0.0.0.0:${API_PORT}" \
-     --master="${MASTER_ADDR}" \
-     --public-master="${API_SCHEME}://${PUBLIC_MASTER_HOST}" \
-     --hostname="${KUBELET_HOST}" \
-     --volume-dir="${VOLUME_DIR}" \
-     --etcd-dir="${ETCD_DATA_DIR}" \
-     --cert-dir="${CERT_DIR}" \
-     --loglevel=4 \
-     --images="${USE_IMAGES}" \
-     --create-certs=false \
-     &> "${LOG_DIR}/openshift.log" &
+	--master-config=${MASTER_CONFIG_DIR}/master-config.yaml \
+	--node-config=${NODE_CONFIG_DIR}/node-config.yaml \
+    --loglevel=4 \
+    &> "${LOG_DIR}/openshift.log" &
 OS_PID=$!
+	
+export HOME="${FAKE_HOME_DIR}"
 
 if [[ "${API_SCHEME}" == "https" ]]; then
-	export CURL_CA_BUNDLE="${CERT_DIR}/ca/cert.crt"
-	export CURL_CERT="${CERT_DIR}/admin/cert.crt"
-	export CURL_KEY="${CERT_DIR}/admin/key.key"
+	export CURL_CA_BUNDLE="${MASTER_CONFIG_DIR}/ca.crt"
+	export CURL_CERT="${MASTER_CONFIG_DIR}/admin.crt"
+	export CURL_KEY="${MASTER_CONFIG_DIR}/admin.key"
 
-	# Make osc use ${CERT_DIR}/admin/.kubeconfig, and ignore anything in the running user's $HOME dir
-	export HOME="${CERT_DIR}/admin"
-	sudo chmod -R a+rwX "${HOME}"
-	export OPENSHIFTCONFIG="${CERT_DIR}/admin/.kubeconfig"
+	# Make osc use ${MASTER_CONFIG_DIR}/admin.kubeconfig, and ignore anything in the running user's $HOME dir
+	export OPENSHIFTCONFIG="${MASTER_CONFIG_DIR}/admin.kubeconfig"
+	sudo chmod -R a+rwX "${OPENSHIFTCONFIG}"
 	echo "[INFO] To debug: export OPENSHIFTCONFIG=$OPENSHIFTCONFIG"
 fi
 
@@ -253,12 +266,12 @@ echo "Log in as 'e2e-user' to see the 'test' project."
 
 # install the router
 echo "[INFO] Installing the router"
-openshift admin router --create --credentials="${CERT_DIR}/openshift-router/.kubeconfig" --images="${USE_IMAGES}"
+openshift admin router --create --credentials="${MASTER_CONFIG_DIR}/openshift-router.kubeconfig" --images="${USE_IMAGES}"
 
 # install the registry. The --mount-host option is provided to reuse local storage.
 echo "[INFO] Installing the registry"
 # TODO: add --images="${USE_IMAGES}" when the Docker registry is built alongside OpenShift
-openshift admin registry --create --credentials="${CERT_DIR}/openshift-registry/.kubeconfig" --mount-host="/tmp/openshift.local.registry" --images='openshift/origin-${component}:latest'
+openshift admin registry --create --credentials="${MASTER_CONFIG_DIR}/openshift-registry.kubeconfig" --mount-host="/tmp/openshift.local.registry" --images='openshift/origin-${component}:latest'
 
 echo "[INFO] Pre-pulling and pushing ruby-20-centos7"
 docker pull openshift/ruby-20-centos7:latest

hack/test-extended.sh
@@ -29,11 +29,13 @@ export BASETMPDIR="${TMPDIR}/openshift-extended-tests"
 rm -rf ${BASETMPDIR} && mkdir -p ${BASETMPDIR}
 
 # Setup directories and certificates for 'curl'
-export CERT_DIR="${BASETMPDIR}/cert"
-export CURL_CA_BUNDLE="${CERT_DIR}/ca/cert.crt"
-export CURL_CERT="${CERT_DIR}/admin/cert.crt"
-export CURL_KEY="${CERT_DIR}/admin/key.key"
-export OPENSHIFTCONFIG="${CERT_DIR}/admin/.kubeconfig"
+export SERVER_CONFIG_DIR="${BASETMPDIR}/openshift.local.config"
+export MASTER_CONFIG_DIR="${SERVER_CONFIG_DIR}/master"
+export NODE_CONFIG_DIR="${SERVER_CONFIG_DIR}/node-127.0.0.1"
+export CURL_CA_BUNDLE="${MASTER_CONFIG_DIR}/ca.crt"
+export CURL_CERT="${MASTER_CONFIG_DIR}/admin.crt"
+export CURL_KEY="${MASTER_CONFIG_DIR}/admin.key"
+export OPENSHIFTCONFIG="${MASTER_CONFIG_DIR}/admin.kubeconfig"
 export OPENSHIFT_ON_PANIC=crash
 
 cleanup() {
@@ -60,7 +62,7 @@ start_server() {
   echo "[INFO] Create certificates for the OpenShift master"
   env "PATH=${PATH}" openshift admin create-master-certs \
     --overwrite=false \
-    --cert-dir="${CERT_DIR}" \
+    --cert-dir="${MASTER_CONFIG_DIR}" \
     --hostnames="${SERVER_HOSTNAME_LIST}" \
     --master="https://${OS_MASTER_ADDR}" \
     --public-master="https://${OS_MASTER_ADDR}"
@@ -68,27 +70,34 @@ start_server() {
   echo "[INFO] Create certificates for the OpenShift node"
   env "PATH=${PATH}" openshift admin create-node-config \
     --listen="https://0.0.0.0:10250" \
-    --node-dir="${CERT_DIR}/node-127.0.0.1" \
+    --node-dir="${NODE_CONFIG_DIR}" \
     --node="127.0.0.1" \
     --hostnames="${SERVER_HOSTNAME_LIST}" \
     --master="https://${OS_MASTER_ADDR}" \
-    --node-client-certificate-authority="${CERT_DIR}/ca/cert.crt" \
-    --certificate-authority="${CERT_DIR}/ca/cert.crt" \
-    --signer-cert="${CERT_DIR}/ca/cert.crt" \
-    --signer-key="${CERT_DIR}/ca/key.key" \
-    --signer-serial="${CERT_DIR}/ca/serial.txt"
+    --node-client-certificate-authority="${MASTER_CONFIG_DIR}/ca.crt" \
+    --certificate-authority="${MASTER_CONFIG_DIR}/ca.crt" \
+    --signer-cert="${MASTER_CONFIG_DIR}/ca.crt" \
+    --signer-key="${MASTER_CONFIG_DIR}/ca.key" \
+    --signer-serial="${MASTER_CONFIG_DIR}/ca.serial.txt"
 
-  echo "[INFO] Starting OpenShift ..."
-  sudo env "PATH=${PATH}" openshift start \
+osadm create-bootstrap-policy-file --filename="${MASTER_CONFIG_DIR}/policy.json"
+
+  # create openshift config
+  openshift start \
+    --write-config=${SERVER_CONFIG_DIR} \
+    --create-certs=false \
     --listen="https://0.0.0.0:${OS_MASTER_PORT}" \
     --public-master="https://${OS_MASTER_ADDR}" \
     --etcd="http://127.0.0.1:${ETCD_PORT}" \
     --hostname="127.0.0.1" \
-    --create-certs=false \
-    --cert-dir="${CERT_DIR}" \
     --volume-dir="${BASETMPDIR}/volumes" \
     --master="https://${OS_MASTER_ADDR}" \
-    --latest-images \
+    --latest-images 
+
+  echo "[INFO] Starting OpenShift ..."
+  sudo env "PATH=${PATH}" openshift start \
+    --master-config=${MASTER_CONFIG_DIR}/master-config.yaml \
+    --node-config=${NODE_CONFIG_DIR}/node-config.yaml \
     --loglevel=${VERBOSE:-3} &> ${BASETMPDIR}/server.log &
   echo -n $! > ${BASETMPDIR}/server.pid
 }

images/origin/Dockerfile
@@ -17,6 +17,6 @@ RUN ln -s /usr/bin/openshift /usr/bin/osc && \
     ln -s /usr/bin/openshift /usr/bin/openshift-router
 
 ENV HOME /root
-ENV OPENSHIFTCONFIG /var/lib/openshift/openshift.local.certificates/admin/.kubeconfig
+ENV OPENSHIFTCONFIG /var/lib/openshift/openshift.local.config/master/admin.kubeconfig
 WORKDIR /var/lib/openshift
 ENTRYPOINT ["/usr/bin/openshift"]

pkg/cmd/server/admin/create_bootstrappolicy_file.go
@@ -20,7 +20,7 @@ import (
 )
 
 const (
-	DefaultPolicyFile                    = "openshift.local.policy/policy.json"
+	DefaultPolicyFile                    = "openshift.local.config/master/policy.json"
 	CreateBootstrapPolicyFileCommand     = "create-bootstrap-policy-file"
 	CreateBootstrapPolicyFileFullCommand = "openshift admin " + CreateBootstrapPolicyFileCommand
 )

pkg/cmd/server/admin/create_client.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"io"
 	"io/ioutil"
-	"path"
 
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
@@ -59,7 +58,7 @@ func NewCommandCreateClient(commandName string, fullName string, out io.Writer) 
 
 	flags.StringVar(&options.APIServerURL, "master", "https://localhost:8443", "The API server's URL.")
 	flags.StringVar(&options.PublicAPIServerURL, "public-master", "", "The API public facing server's URL (if applicable).")
-	flags.StringVar(&options.APIServerCAFile, "certificate-authority", "openshift.local.certificates/ca/cert.crt", "Path to the API server's CA file.")
+	flags.StringVar(&options.APIServerCAFile, "certificate-authority", "openshift.local.config/master/ca.crt", "Path to the API server's CA file.")
 
 	return cmd
 }
@@ -94,10 +93,10 @@ func (o CreateClientOptions) Validate(args []string) error {
 func (o CreateClientOptions) CreateClientFolder() error {
 	glog.V(2).Infof("creating a .kubeconfig with: %#v", o)
 
-	clientCertFile := path.Join(o.ClientDir, "cert.crt")
-	clientKeyFile := path.Join(o.ClientDir, "key.key")
-	clientCopyOfCAFile := path.Join(o.ClientDir, "ca.crt")
-	kubeConfigFile := path.Join(o.ClientDir, ".kubeconfig")
+	clientCertFile := DefaultCertFilename(o.ClientDir, o.User)
+	clientKeyFile := DefaultKeyFilename(o.ClientDir, o.User)
+	clientCopyOfCAFile := DefaultCAFilename(o.ClientDir, o.User)
+	kubeConfigFile := DefaultKubeConfigFilename(o.ClientDir, o.User)
 
 	createClientCertOptions := CreateClientCertOptions{
 		GetSignerCertOptions: o.GetSignerCertOptions,

commit 532e2f568ebbb0454a327b217a96cd2c24a7e072

vagrant: fix creating minion config when provisioning master
Fixes: 40be29beaabd097788200d8dadbf0a9e35374df7

https://github.com/openshift/origin/pull/1952

vagrant/provision-master.sh
@@ -35,7 +35,6 @@ echo "Generating certs"
 pushd /vagrant
   SERVER_CONFIG_DIR="`pwd`/openshift.local.config"
   MASTER_CONFIG_DIR="${SERVER_CONFIG_DIR}/master"
-  NODE_CONFIG_DIR="${SERVER_CONFIG_DIR}/node-${minion}"
   CERT_DIR="${MASTER_CONFIG_DIR}"
 
   # Master certs
@@ -51,7 +50,7 @@ pushd /vagrant
     ip=${minion_ip_array[$i]}
 
     /usr/bin/openshift admin create-node-config \
-      --node-dir="${NODE_CONFIG_DIR}" \
+      --node-dir="${SERVER_CONFIG_DIR}/node-${minion}" \
       --node="${minion}" \
       --hostnames="${minion},${ip}" \
       --master="https://${MASTER_IP}:8443" \